b2efa7a52e63c2deff2e9199e6b01c3d52b5d780
mevex <mevex98@gmail.com>
2019-12-14 14:15:18 +0000

    Implemented dungeon generator prototype

---
+++ GE1Assignment/Assets/RoomSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Directions
+{
+    right,
+    bottom,
+    left,
+    top
+}
+
+public class RoomSpawner : MonoBehaviour
+{
+    public Directions openingDirections;
+    private RoomsContainer rooms;
+    private bool spawn = true;
+
+    void Start()
+    {
+        rooms = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomsContainer>();
+        Invoke("Spawn", 0.2f);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        if(spawn)
+        {
+            switch (openingDirections)
+            {
+                case Directions.right:
+                    {
+                        // LEFT
+                        int rand = Random.Range(0, rooms.leftRooms.Length);
+                        Instantiate(rooms.leftRooms[rand], transform.position, Quaternion.identity);
+                    }
+                    break;
+                case Directions.bottom:
+                    {
+                        // TOP
+                        int rand = Random.Range(0, rooms.topRooms.Length);
+                        Instantiate(rooms.topRooms[rand], transform.position, Quaternion.identity);
+                    }
+                    break;
+                case Directions.left:
+                    {
+                        // RIGHT
+                        int rand = Random.Range(0, rooms.rightRooms.Length);
+                        Instantiate(rooms.rightRooms[rand], transform.position, Quaternion.identity);
+                    }
+                    break;
+                case Directions.top:
+                    {
+                        // BOTTOM
+                        int rand = Random.Range(0, rooms.bottomRooms.Length);
+                        Instantiate(rooms.bottomRooms[rand], transform.position, Quaternion.identity);
+                    }
+                    break;
+            }
+            spawn = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("SpawnPoint"))
+        {
+            Debug.Log("Collided");
+            spawn = false;
+        }
+    }
+}
---
+++ GE1Assignment/Assets/RoomsContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsContainer : MonoBehaviour
+{
+    public GameObject[] rightRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] topRooms;
+}
